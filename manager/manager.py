d86b31fc91efd840148c8d3bb1aee8147df89841
Klaudia Ryciuk <rinrearoa@gmail.com>
2023-01-29 22:00:52 +0100

    refactor: divide code into modules

--- manager/manager.py
+++ manager/manager.py
@@ -1,5 +1,5 @@
 from menu.menu import Menu
-from file_handler.file_handler import FileOperations
+from rot_operations.rot_operations import RotOperations
 
 
 class Manager:
@@ -17,95 +17,11 @@ class Manager:
         choice = int(input())
         match choice:
             case 1:
-                return Manager.encoding()
+                return RotOperations.encoding()
             case 2:
-                return Manager.decoding()
+                return RotOperations.decoding()
             case 3:
                 # del Buffer
                 exit()
-            case _:
-                print("Incorrect choice")
-
-    @staticmethod
-    def encoding():
-        Menu.rot_choice()
-        choice = int(input())
-        match choice:
-            case 1:
-                return Manager.encoding_rot13()
-            case 2:
-                return Manager.encoding_rot47()
-            case _:
-                print("Incorrect choice")
-
-    @staticmethod
-    def decoding():
-        Menu.rot_choice()
-        choice = int(input())
-        match choice:
-            case 1:
-                return Manager.decoding_rot13()
-            case 2:
-                return Manager.decoding_rot47()
-            case _:
-                print("Incorrect choice")
-
-    @staticmethod
-    def encoding_rot13():
-        file_to_encrypt = input("Enter the name of the file that will be encrypted.\n")
-        Menu.file_choice()
-        choice = int(input())
-        if choice == 2:
-            new_file_name = input("Enter the file name...\n")
-        match choice:
-            case 1:
-                FileOperations.encrypt_add_rot13(file_to_encrypt)
-            case 2:
-                FileOperations.encrypt_create_rot13(file_to_encrypt, new_file_name)
-            case _:
-                print("Incorrect choice")
-
-    @staticmethod
-    def encoding_rot47():
-        file_to_encrypt = input("Enter the name of the file that will be encrypted.\n")
-        Menu.file_choice()
-        choice = int(input())
-        if choice == 2:
-            new_file_name = input("Enter the file name...\n")
-        match choice:
-            case 1:
-                FileOperations.encrypt_add_rot47(file_to_encrypt)
-            case 2:
-                FileOperations.encrypt_create_rot47(file_to_encrypt, new_file_name)
-            case _:
-                print("Incorrect choice")
-
-    @staticmethod
-    def decoding_rot13():
-        file_to_decrypt = input("Enter the name of the file that will be decrypted.\n")
-        Menu.file_choice()
-        choice = int(input())
-        if choice == 2:
-            new_file_name = input("Enter the file name...\n")
-        match choice:
-            case 1:
-                FileOperations.decrypt_add_rot13(file_to_decrypt)
-            case 2:
-                FileOperations.decrypt_create_rot13(file_to_decrypt, new_file_name)
-            case _:
-                print("Incorrect choice")
-
-    @staticmethod
-    def decoding_rot47():
-        file_to_decrypt = input("Enter the name of the file that will be decrypted.\n")
-        Menu.file_choice()
-        choice = int(input())
-        if choice == 2:
-            new_file_name = input("Enter the file name...\n")
-        match choice:
-            case 1:
-                FileOperations.encrypt_add_rot47(file_to_decrypt)
-            case 2:
-                FileOperations.encrypt_create_rot47(file_to_decrypt, new_file_name)
             case _:
                 print("Incorrect choice")
---
+++ rot_operations/rot_operations.py
@@ -0,0 +1,88 @@
+from menu.menu import Menu
+from file_handler.file_handler import FileOperations
+
+
+class RotOperations:
+    @staticmethod
+    def encoding():
+        Menu.rot_choice()
+        choice = int(input())
+        match choice:
+            case 1:
+                return RotOperations.encoding_rot13()
+            case 2:
+                return RotOperations.encoding_rot47()
+            case _:
+                print("Incorrect choice")
+
+    @staticmethod
+    def decoding():
+        Menu.rot_choice()
+        choice = int(input())
+        match choice:
+            case 1:
+                return RotOperations.decoding_rot13()
+            case 2:
+                return RotOperations.decoding_rot47()
+            case _:
+                print("Incorrect choice")
+
+    @staticmethod
+    def encoding_rot13():
+        file_to_encrypt = input("Enter the name of the file that will be encrypted.\n")
+        Menu.file_choice()
+        choice = int(input())
+        if choice == 2:
+            new_file_name = input("Enter the file name...\n")
+        match choice:
+            case 1:
+                FileOperations.encrypt_add_rot13(file_to_encrypt)
+            case 2:
+                FileOperations.encrypt_create_rot13(file_to_encrypt, new_file_name)
+            case _:
+                print("Incorrect choice")
+
+    @staticmethod
+    def encoding_rot47():
+        file_to_encrypt = input("Enter the name of the file that will be encrypted.\n")
+        Menu.file_choice()
+        choice = int(input())
+        if choice == 2:
+            new_file_name = input("Enter the file name...\n")
+        match choice:
+            case 1:
+                FileOperations.encrypt_add_rot47(file_to_encrypt)
+            case 2:
+                FileOperations.encrypt_create_rot47(file_to_encrypt, new_file_name)
+            case _:
+                print("Incorrect choice")
+
+    @staticmethod
+    def decoding_rot13():
+        file_to_decrypt = input("Enter the name of the file that will be decrypted.\n")
+        Menu.file_choice()
+        choice = int(input())
+        if choice == 2:
+            new_file_name = input("Enter the file name...\n")
+        match choice:
+            case 1:
+                FileOperations.decrypt_add_rot13(file_to_decrypt)
+            case 2:
+                FileOperations.decrypt_create_rot13(file_to_decrypt, new_file_name)
+            case _:
+                print("Incorrect choice")
+
+    @staticmethod
+    def decoding_rot47():
+        file_to_decrypt = input("Enter the name of the file that will be decrypted.\n")
+        Menu.file_choice()
+        choice = int(input())
+        if choice == 2:
+            new_file_name = input("Enter the file name...\n")
+        match choice:
+            case 1:
+                FileOperations.encrypt_add_rot47(file_to_decrypt)
+            case 2:
+                FileOperations.encrypt_create_rot47(file_to_decrypt, new_file_name)
+            case _:
+                print("Incorrect choice")
