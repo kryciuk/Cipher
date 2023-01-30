eb3d3f7f176a3b920f610c5a1358387f07af26e6
Klaudia Ryciuk <rinrearoa@gmail.com>
2023-01-30 20:14:28 +0100

    feat: change .txt to .json

--- file_handler/file_handler.py
+++ file_handler/file_handler.py
@@ -1,44 +1,46 @@
 from functionality.rot import Rot47, Rot13
 from buffer.buffer import Buffer
+import json
 
 
 class FileOperations:
     @staticmethod
     def encrypt(choice, file_name, rot, new_file_name=None):
-        with open(file_name, "a+") as file:
-            file.seek(0)
-            lines = file.read()
+        with open(file_name, "r+") as file:
+            lines = json.load(file)
             if rot == 1:
                 lines_encoded = Rot13.encode(lines)
-                _type = "Rot13"
+                rot_type = "Rot13"
             elif rot == 2:
                 lines_encoded = Rot47.encode(lines)
-                _type = "Rot47"
+                rot_type = "Rot47"
             if choice == 1:
-                file.write(lines_encoded)
+                file.seek(0)
+                json.dump(lines + " " + lines_encoded, file)
             elif choice == 2:
                 with open(new_file_name, "w+") as new_file:
-                    new_file.write(lines_encoded)
+                    json.dump(lines_encoded, new_file)
             Buffer.buffer.append(
-                {"type": _type, "text": lines_encoded, "status": "encrypted"}
+                {"type": rot_type, "text": lines_encoded, "status": "encrypted"}
             )
 
     @staticmethod
     def decrypt(choice, file_name, rot, new_file_name=None):
         with open(file_name, "a+") as file:
             file.seek(0)
-            lines = file.read()
+            lines = json.load(file)
             if rot == 1:
                 lines_decoded = Rot13.decode(lines)
-                _type = "Rot13"
+                rot_type = "Rot13"
             elif rot == 2:
-                lines_decoded = Rot47.decode(lines)
-                _type = "Rot47"
+                lines_decoded = Rot47.encode(lines)
+                rot_type = "Rot47"
             if choice == 1:
-                file.write(lines_decoded)
+                file.seek(0)
+                json.dump(lines + " " + lines_decoded, file)
             elif choice == 2:
                 with open(new_file_name, "w+") as new_file:
-                    new_file.write(lines_decoded)
+                    json.dump(lines_decoded, new_file)
             Buffer.buffer.append(
-                {"type": _type, "text": lines_decoded, "status": "decrypted"}
+                {"type": rot_type, "text": lines_decoded, "status": "decrypted"}
             )
